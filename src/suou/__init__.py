17c5acb1bbfb425a3a1f5638655998cca1469320
Yusur Princeps <sakuragasaki46@gmail.com>
2025-09-30 20:40:45 +0200

    0.6.2 release

--- src/suou/__init__.py
+++ src/suou/__init__.py
@@ -36,7 +36,7 @@ from .validators import matches
 from .redact import redact_url_password
 from .http import WantsContentType
 
-__version__ = "0.6.2.rc1"
+__version__ = "0.6.2"
 
 __all__ = (
     'ConfigOptions', 'ConfigParserConfigSource', 'ConfigSource', 'ConfigValue',
