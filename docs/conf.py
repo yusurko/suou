1b03f3b2e9080cf3b11757e912d6194cd8defcd4
Yusur Princeps <sakuragasaki46@gmail.com>
2025-10-10 20:24:38 +0200

    again fix

--- docs/conf.py
+++ docs/conf.py
@@ -35,4 +35,5 @@ autodoc_mock_imports = [
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
+html_theme_path = ["_themes", ]
 html_static_path = ['_static']
